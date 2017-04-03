4989d75e7077495a2de77dba1d9f75fe9720e12d
Luis Ruvalcaba <lruval@microsoft.com>
2017-04-03 17:35:37 -0500

    Reto 4

---
+++ Reto4/Start/Reto4/Services/ServiceHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
+
+namespace Reto4.Services
+{
+    public class ServiceHelper
+    {
+        MobileServiceClient clienteServicio = new MobileServiceClient(@"http://xamarinchampions.azurewebsites.net");
+
+        private IMobileServiceTable<TorneoItem> _TorneoItemTable;
+    }
+}
---
+++ Reto4/Start/Reto4/Services/TorneoItem.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace Reto4.Services
+{
+    public class TorneoItem
+    {
+        private string _id;
+        private string _email;
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        [JsonProperty(PropertyName = "Email")]
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+            }
+        }
+
+        public string Reto { get; set; }
+        public string DeviceId { get; set; }
+
+        [Version]
+        public string Version { get; set; }
+
+    }
+}
