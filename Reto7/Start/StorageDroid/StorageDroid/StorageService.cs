1e4a303bba4c23ec86cfa1eb8e2edee0112b2d8f
Luis Ruvalcaba Sanchez <lruval@microsoft.com>
2017-05-22 20:49:44 -0500

    Reto7

---
+++ Reto7/Start/StorageDroid/StorageDroid/StorageService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+
+namespace StorageDroid
+{
+    public class StorageService
+    {
+        public StorageService()
+        {
+        }
+
+        public  async Task performBlobOperation(string participante)
+        {
+            try
+            {
+                string blobSAS = "https://torneoxamarin.blob.core.windows.net/devs/" + participante + ".txt?sv=2016-05-31&ss=b&srt=sco&sp=wlac&se=2017-07-01T15:00:00Z&st=2017-05-22T18:00:00Z&sip=0.0.0.0-255.255.255.255&spr=https,http&sig=UDaqkQEJrd8E%2FCPhLwWmrM3ZZobdTlTqQadVIy67pXc%3D";
+                CloudBlockBlob blob = new CloudBlockBlob(new Uri(blobSAS));
+
+                string blobContent = "Ingresa tu nombre y país";
+                MemoryStream msWrite = new MemoryStream(Encoding.UTF8.GetBytes(blobContent));
+                msWrite.Position = 0;
+                using (msWrite)
+                {
+                   await blob.UploadFromStreamAsync(msWrite);
+                }
+            }
+            catch (Exception exc)
+            {
+                string msgError = exc.Message;
+            }
+
+        }
+    }
+}
+
