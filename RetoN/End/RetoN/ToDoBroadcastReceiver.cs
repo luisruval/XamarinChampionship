1f05b72ad62a677db936ecf731091fb2ab8ee0c0
Luis Ruvalcaba Sanchez <lruval@microsoft.com>
2017-06-16 00:48:37 -0500

    Reto N - Code Complete

---
+++ RetoN/End/RetoN/ToDoBroadcastReceiver.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Gcm.Client;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json.Linq;
+
+[assembly: Permission(Name = "@PACKAGE_NAME@.permission.C2D_MESSAGE")]
+[assembly: UsesPermission(Name = "@PACKAGE_NAME@.permission.C2D_MESSAGE")]
+[assembly: UsesPermission(Name = "com.google.android.c2dm.permission.RECEIVE")]
+
+//GET_ACCOUNTS is only needed for android versions 4.0.3 and below
+[assembly: UsesPermission(Name = "android.permission.GET_ACCOUNTS")]
+[assembly: UsesPermission(Name = "android.permission.INTERNET")]
+[assembly: UsesPermission(Name = "android.permission.WAKE_LOCK")]
+
+namespace RetoN
+{
+    [BroadcastReceiver(Permission = Gcm.Client.Constants.PERMISSION_GCM_INTENTS)]
+    [IntentFilter(new string[] { Gcm.Client.Constants.INTENT_FROM_GCM_MESSAGE },
+     Categories = new string[] { "@PACKAGE_NAME@" })]
+    [IntentFilter(new string[] { Gcm.Client.Constants.INTENT_FROM_GCM_REGISTRATION_CALLBACK },
+     Categories = new string[] { "@PACKAGE_NAME@" })]
+    [IntentFilter(new string[] { Gcm.Client.Constants.INTENT_FROM_GCM_LIBRARY_RETRY },
+ Categories = new string[] { "@PACKAGE_NAME@" })]
+    public class ToDoBroadcastReceiver : GcmBroadcastReceiverBase<PushHandlerService>
+    {
+        // Set the Google app ID.
+        public static string[] senderIDs = new string[] { "511600433704" };
+    }
+
+
+    // The ServiceAttribute must be applied to the class.
+    [Service]
+    public class PushHandlerService : GcmServiceBase
+    {
+        public static string RegistrationID { get; private set; }
+
+        public PushHandlerService() : base(ToDoBroadcastReceiver.senderIDs) { }
+
+        protected override void OnRegistered(Context context, string registrationId)
+        {
+            System.Diagnostics.Debug.WriteLine("The device has been registered with GCM.", "Success!");
+
+            // Get the MobileServiceClient from the current activity instance.
+            MobileServiceClient client = ToDoActivity.CurrentActivity.CurrentClient;
+            var push = client.GetPush();
+
+            // Define a message body for GCM.
+            const string templateBodyGCM = "{\"data\":{\"message\":\"$(messageParam)\"}}";
+
+            // Define the template registration as JSON.
+            JObject templates = new JObject();
+            templates["genericMessage"] = new JObject
+             {
+               {"body", templateBodyGCM }
+             };
+
+            try
+            {
+                // Make sure we run the registration on the same thread as the activity, 
+                // to avoid threading errors.
+                ToDoActivity.CurrentActivity.RunOnUiThread(
+
+                    // Register the template with Notification Hubs.
+                    async () => await push.RegisterAsync(registrationId, templates));
+
+                System.Diagnostics.Debug.WriteLine(
+                    string.Format("Push Installation Id", push.InstallationId.ToString()));
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(
+                    string.Format("Error with Azure push registration: {0}", ex.Message));
+            }
+        }
+
+        protected override void OnMessage(Context context, Intent intent)
+        {
+            string message = string.Empty;
+
+            // Extract the push notification message from the intent.
+            if (intent.Extras.ContainsKey("message"))
+            {
+                message = intent.Extras.Get("message").ToString();
+                var title = "Championship - Reto N";
+
+                // Create a notification manager to send the notification.
+                var notificationManager =
+                    GetSystemService(Context.NotificationService) as NotificationManager;
+
+                // Create a new intent to show the notification in the UI. 
+                PendingIntent contentIntent =
+                    PendingIntent.GetActivity(context, 0,
+                    new Intent(this, typeof(ToDoActivity)), 0);
+
+                // Create the notification using the builder.
+                var builder = new Notification.Builder(context);
+                builder.SetAutoCancel(true);
+                builder.SetContentTitle(title);
+                builder.SetContentText(message);
+                builder.SetSmallIcon(Resource.Drawable.ic_launcher);
+                builder.SetContentIntent(contentIntent);
+                var notification = builder.Build();
+
+                // Display the notification in the Notifications Area.
+                notificationManager.Notify(1, notification);
+
+            }
+        }
+
+        protected override void OnUnRegistered(Context context, string registrationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnError(Context context, string errorId)
+        {
+            System.Diagnostics.Debug.WriteLine(
+                string.Format("Error occurred in the notification: {0}.", errorId));
+        }
+
+    }
+}
